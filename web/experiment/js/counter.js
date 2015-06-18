74c968343859a9901a7401a4bf595808a91859ed
Jingke Song <jingke.song@sap.com>
2015-06-18 10:16:58 +0800

    refine counter exmaple

--- web/experiment/js/counter.js
+++ web/experiment/js/counter.js
@@ -1,8 +1,28 @@
+// define a counter
+var counter = (function() {
+  'use strict';  
+  var _counter = 0;
+  
+  return function() {
+    return ++_counter;
+  };
+})();
+
+// demo
+document.getElementById('count').onclick = function() {
+  'use strict';  
+  document.getElementById('demo').innerHTML = counter();
+};
+
+// define a counter factory
 var makeCounter = function() {
+  'use strict';  
   var privateCounter = 0;
+  
   function changeBy(val) {
     privateCounter += val;
   }
+  
   return {
     increment: function() {
       changeBy(1);
@@ -13,15 +33,16 @@ var makeCounter = function() {
     value: function() {
       return privateCounter;
     }
-  }  
+  };
 };
 
+// demo
 var Counter1 = makeCounter();
 var Counter2 = makeCounter();
-alert(Counter1.value()); /* 提示 0 */
+//alert(Counter1.value()); /* 提示 0 */
 Counter1.increment();
 Counter1.increment();
-alert(Counter1.value()); /* 提示 2 */
+//alert(Counter1.value()); /* 提示 2 */
 Counter1.decrement();
-alert(Counter1.value()); /* 提示 1 */
-alert(Counter2.value()); /* 提示 0 */
+//alert(Counter1.value()); /* 提示 1 */
+//alert(Counter2.value()); /* 提示 0 */
